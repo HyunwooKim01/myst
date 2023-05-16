a7a85731e20cc69d012a2057d64ffd13575bb56e
HyunwooKim01 <126743699+HyunwooKim01@users.noreply.github.com>
2023-05-16 11:30:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,9 @@ st.caption("this is the caption")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
+st.checkbox('yes')
+st.button('Touch')
+st.selecbox('Pick your gender',['Men','Women'])
+st.select_slider('your older',0,100)
+
+
